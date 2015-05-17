7dc867d6658826e9773ec32b1bca235612af42af
Robin Lee Powell <rlpowell@digitalkingdom.org>
2015-05-17 10:22:35 -0700

    All the way working, albeit not pretty.

--- mw.hs
+++ mw.hs
@@ -2,15 +2,11 @@ module Main where
 import Haste
 import Haste.DOM
 import Haste.Events
-import Control.Monad.IO.Class
 import Data.List
+import Text.Printf
+import Debug.Trace
 
 type LocationName = String
-
--- data Location = Location {
---     name :: LocationName
---   , connections :: [Connection]
--- } deriving (Show, Eq)
 
 data Connection = Connection {
     origin :: LocationName
@@ -27,6 +23,52 @@ data ConnectionType =
   | Boat
   deriving (Show, Eq)
 
+data Path = Path {
+    pconns :: [Connection]
+  , pdest :: LocationName
+} deriving (Show, Eq)
+
+reducePaths :: [LocationName] -> [Path] -> [Path]
+reducePaths dests (path:paths) =
+  if (pdest path) `elem` dests then
+    [] ++ reducePaths dests paths
+  else
+    [path] ++ reducePaths (dests ++ [(pdest path)]) paths
+reducePaths _ [] = []
+
+expandPaths :: [Path] -> [Path]
+expandPaths paths =
+  paths ++ concatMap addPaths paths
+
+addPaths :: Path -> [Path]
+addPaths start =
+  map (extendMatchingPath start) connections
+
+extendMatchingPath :: Path -> Connection -> Path
+extendMatchingPath path conn =
+  if (pdest path) == (origin conn) then
+    Path { pconns=((pconns path) ++ [conn]), pdest=(destination conn) }
+  else
+    path
+
+findPath :: LocationName -> LocationName -> [Path] -> Path
+findPath start end paths = trace (printf "In findPath: start: %s, end: %s, paths: %s" start end (show paths)) $
+  case maybePath of
+    Nothing ->
+      -- If we didn't add any new destinations, we've exhausted the
+      -- search tree; give up
+      if paths == newPaths then
+        error $ printf "No path to %s found!" end
+      else
+        findPath start end newPaths
+    Just path -> path
+  where maybePath = find (\x -> (pdest x) == end) paths
+        -- We inject the start path, even if we have other paths,
+        -- every time.  This way we don't have to make any
+        -- decisions, and it's pretty cheap.
+        newPaths = reducePaths [start] $ expandPaths $ Path { pdest=start, pconns=[] } : paths
+
+connections :: [Connection]
 connections = [
     Connection { origin="Balmora", destination="Vivec", ctype=Mage }
   , Connection { origin="Balmora", destination="Sadrith Mora", ctype=Mage }
@@ -36,6 +78,7 @@ connections = [
   , Connection { origin="Vivec", destination="Balmora", ctype=Mage }
   ]
 
+locations :: [LocationName]
 locations = [
     "Balmora"
   , "Sadrith Mora"
@@ -43,50 +86,19 @@ locations = [
   , "Vivec"
   ]
 
--- locations = [
---     Location { name="Balmora", connections=[
---           Connection { destination="Vivec", ctype=Mage }
---         , Connection { destination="Sadrith Mora", ctype=Mage }
---     ] }
---   , Location { name="Sadrith Mora", connections=[
---           Connection { destination="Vivec", ctype=Mage }
---         , Connection { destination="Balmora", ctype=Mage }
---         , Connection { destination="Tel Mora", ctype=Boat }
---     ] }
---   , Location { name="Tel Mora", connections=[
---     ] }
---   , Location { name="Vivec", connections=[ Connection { destination="Balmora", ctype=Mage } ] }
---   ]
-
 makeOption :: String -> IO Elem
 makeOption opt = do
-  elem <- newElem "option"
-  set elem [attr "value" =: opt]
+  optElem <- newElem "option"
+  set optElem [attr "value" =: opt]
   inner <- newTextElem opt
-  addChild inner elem
-  return elem
+  addChild inner optElem
+  return optElem
 
+makeLocationElems :: IO [Elem]
 makeLocationElems = mapM makeOption locations
 
 addChildren :: Elem -> [Elem] -> IO ()
-addChildren parent children = sequence_ [addChild c parent | c <- children]
-
-findMatchingConn :: LocationName -> LocationName -> [Connection] -> Maybe Connection
-findMatchingConn start end conns = find (\x -> ((origin x) == start) && ((destination x) == end)) conns
-
-findConnsByOrigin :: LocationName -> [Connection] -> [Connection]
-findConnsByOrigin wanted conns = filter (\x -> (origin x) == wanted) conns
-
-findPath :: LocationName -> LocationName -> Integer -> [Connection] -> [Connection]
-findPath originName destName depth path =
-    if depth > 10 then
-      []
-    else
-      case findMatchingConn originName destName connections of
-        Just conn -> path ++ [conn]
-        Nothing -> case find (/= []) (map (\x -> findPath (destination x) destName (depth + 1) (path ++ [x])) (findConnsByOrigin originName connections)) of
-          Nothing -> []
-          Just found -> found
+addChildren parent childs = sequence_ [addChild c parent | c <- childs]
 
 handleFind :: Elem -> Elem -> Elem -> EventData BasicEvent -> IO ()
 handleFind start end result event = do
@@ -97,14 +109,14 @@ handleFind start end result event = do
       (Nothing, _) -> error "Couldn't get value for starting point."
       (_, Nothing) -> error "Couldn't get value for ending point."
       (Just startName, Just endName) -> do
-        textbit <- newTextElem (show $ findPath startName endName 0 [])
+        textbit <- newTextElem (show $ findPath startName endName [])
         addChild textbit result
         preventDefault
 
 handleSelection :: Elem -> Elem -> Elem -> IO ()
 handleSelection start end result = do
-  onEvent start Change $ handleFind start end result
-  onEvent end Change $ handleFind start end result
+  _ <- onEvent start Change $ handleFind start end result
+  _ <- onEvent end Change $ handleFind start end result
   return ()
 
 main :: IO ()
@@ -123,41 +135,3 @@ main = do
           addChildren end endLocationElems
           handleSelection start end result
           return ()
-    
-    
-
-{-
-main = withElems ["a","b","op","result"] calculator
-
-calculator [a,b,op,result] = do
-    onEvent a  KeyUp $ \_ -> recalculate
-    onEvent b  KeyUp $ \_ -> recalculate
-    onEvent op Change $ \_ -> recalculate
-  where
-    recalculate = do
-      ma <- getValue a
-      mb <- getValue b
-      Just op' <- getValue op
-      case (ma, mb) of
-        (Just a', Just b') -> setProp result "innerHTML" (toString $ calc op' a' b')
-        _                  -> return ()
-
-    calc "+" = (+)
-    calc "-" = (-)
-    calc "*" = (*)
-    calc "/" = (/)
-    calc _   = \_ _ -> 0 :: Double
-
-main :: IO ()
-main = do
-  ul <- elemsByQS document "ul#demo-list"
-  case ul of
-    (el:_) -> mapQS_ document "#demo-list li" (handleRemove el)
-    _      -> error "Element 'ul#demo-list' not found"
-
-handleRemove :: Elem -> Elem -> IO HandlerInfo
-handleRemove ul li = do
-  onEvent li Click $ \_ -> do
-    removeChild li ul
-    preventDefault
--}
